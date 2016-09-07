7667d3f8ce9916a3ff09b383877108e006cbeb1e
Joeri Sebrechts <joeri@sebrechts.net>
2016-09-07 22:32:12 +0200

    navigation to older posts

--- app/controllers/Page.php
+++ app/controllers/Page.php
@@ -7,7 +7,9 @@ class Page {
 
   static function index() {
     if (isset($_GET["login"])) _::login();
-    return self::posts(Post::get(false, 5));
+    return self::posts(
+      Post::get(isset($_GET["from"]) ? $_GET["from"] : false, 5)
+    );
   }
 
   static function post($created) {
@@ -18,17 +20,20 @@ class Page {
     header("HTTP/1.0 404 Not Found");
     return _::render("views/page.phtml", [
       "title" => "404",
-      "body" => 'Oops, seems you backed into a corner there.'
+      "body" => "Oops, seems you backed into a corner there."
     ]);
   }
 
   private static function posts($posts) {
+    $older = (end($posts)->created-1);
     return _::render("views/page.phtml", [
       "title" => Setting::get("title"),
       "body" => array_reduce($posts,
         function($str, $post) {
-          return $str._::render("views/post.phtml", ["post" => $post]); }
-      )
+          return $str._::render("views/post.phtml", ["post" => $post]);
+        }
+      ).(((count($posts) == 5) && Post::get($older, 1)) ?
+          '<br><p><a href="/?from='.$older.'">Older</a></p>' : "")
     ]);
   }
 
